46d045eba3985532c05eb4be924975548d2d63b9
Christopher-Coombes <cjbcoombes@gmail.com>
2024-04-09 22:53:11 -0400

    small prompt fix

--- generatestub.py
+++ generatestub.py
@@ -2,7 +2,7 @@ from assignmentdata import *
 
 def generate_stub(aspath, outpath):
     with open(aspath) as asfile, open(outpath, mode="w") as outfile:
-        assignment = AssignmentStatement(json.load(asfile))
+        assignment = AssignmentStatement(json.load(asfile)["assignment"])
         outstr = f"{assignment.lang} \n\n"
         i = 0
         for prob in assignment.problems:
@@ -14,4 +14,4 @@ def generate_stub(aspath, outpath):
         outfile.write(outstr)
 
 # Example
-# generate_stub("example-problem-statement.json", "example-stub.rkt")
+# generate_stub("../feedbot-submissions/tests/f1-f23-hw7/config.json", "example-stub.rkt")
